fc6a2eb9863b4576741e71639f21804da3650a63
Tim Child <timjchild@gmail.com>
2025-03-30 14:50:59 -0700

    test getting_tools

--- mcp-client/mcp_client/multi_client.py
+++ mcp-client/mcp_client/multi_client.py
@@ -1,5 +1,38 @@
+import asyncio
+
 from langchain_core.tools import BaseTool
-from langchain_mcp_adapters.client import MultiServerMCPClient, SSEConnection, StdioConnection
+from langchain_mcp_adapters.client import (
+    MultiServerMCPClient,
+    SSEConnection,
+    StdioConnection,
+)
+from langchain_mcp_adapters.tools import load_mcp_tools
+from mcp import ClientSession
+
+
+class LCClientPatch(MultiServerMCPClient):
+    initialize_timeout_s: float = 5
+
+    # added timeout on intiaializing a session
+    async def _initialize_session_and_load_tools(
+        self, server_name: str, session: ClientSession
+    ) -> None:
+        """Initialize a session and load tools from it.
+
+        Args:
+            server_name: Name to identify this server connection
+            session: The ClientSession to initialize
+        """
+        # Initialize the session
+        try:
+            await asyncio.wait_for(session.initialize(), timeout=self.initialize_timeout_s)
+        except asyncio.TimeoutError:
+            raise RuntimeError("Failed to initialize session within timeout")
+        self.sessions[server_name] = session
+
+        # Load tools from this server
+        server_tools = await load_mcp_tools(session)
+        self.server_name_to_tools[server_name] = server_tools
 
 
 class MultiMCPClient:
@@ -10,7 +43,7 @@ class MultiMCPClient:
             connections: A dictionary mapping server names to connection configurations.
                 Each configuration can be either a StdioConnection or SSEConnection.
         """
-        self.lc_client = MultiServerMCPClient(connections=connections)
+        self.lc_client = LCClientPatch(connections=connections)
 
     async def __aenter__(self) -> "MultiMCPClient":
         """Connects to all servers during context."""
@@ -24,4 +57,5 @@ class MultiMCPClient:
     async def get_tools(self) -> list[BaseTool]:
         """Get all tools available from all connected servers."""
         # NOTE: lc loads on initial connection, so don't need to await here (in general it would be awaited though)
-        return self.lc_client.get_tools()
+        async with self.lc_client:
+            return self.lc_client.get_tools()
