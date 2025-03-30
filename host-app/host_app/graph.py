880824c94785a8fca6beeb09417be3b58b80371f
Tim Child <timjchild@gmail.com>
2025-03-30 13:37:19 -0700

    add very basic graph test

--- host-app/host_app/graph.py
+++ host-app/host_app/graph.py
@@ -1,1 +1,43 @@
 """Regular graph version of langgraph."""
+
+from langchain_core.messages import AIMessage, ToolMessage
+from langgraph.graph import StateGraph
+from langgraph.graph.graph import CompiledGraph
+from mcp_client import MultiMCPClient
+from pydantic import BaseModel
+
+
+class GraphRunner:
+    def __init__(self, mcp_client: MultiMCPClient) -> None:
+        self.mcp_client = mcp_client
+        self.graph: CompiledGraph = make_graph()
+
+
+class FullState(BaseModel):
+    question: str
+    response_messages: list[AIMessage | ToolMessage] = []
+
+
+class InputState(BaseModel):
+    question: str
+
+
+class OutputState(BaseModel):
+    response_messages: list[AIMessage | ToolMessage]
+
+
+def process(inputs: InputState) -> OutputState:
+    return OutputState(response_messages=[AIMessage(content=f"Received: {inputs.question}")])
+
+
+def make_graph(checkpointer: None = None) -> CompiledGraph:
+    graph = StateGraph(state_schema=FullState)
+
+    graph.add_node("process", process)
+
+    graph.set_entry_point("process")
+    graph.set_finish_point("process")
+    compiled_graph = graph.compile(
+        checkpointer=checkpointer, interrupt_before=None, interrupt_after=None, debug=True
+    )
+    return compiled_graph
---
+++ host-app/tests/test_graph.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+import pytest
+from langgraph.graph.graph import CompiledGraph
+
+from host_app.graph import InputState, OutputState, make_graph
+
+
+def test_compile_graph():
+    graph = make_graph()
+    assert isinstance(graph, CompiledGraph)
+
+
+@pytest.fixture(scope="module")
+def graph() -> CompiledGraph:
+    return make_graph()
+
+
+async def test_invoke_graph(graph: CompiledGraph):
+    result: dict[str, Any] = await graph.ainvoke(input=InputState(question="Hello"))
+
+    validated: OutputState = OutputState.model_validate(result)
+    assert validated.response_messages[0].content == "Received: Hello"
