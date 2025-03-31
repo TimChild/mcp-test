e453451fa0016d010519dbc7505fbe6b73eac2ac
Tim Child <timjchild@gmail.com>
2025-03-31 09:52:56 -0700

    add tests for memory store

--- host-app/tests/test_graph.py
+++ host-app/tests/test_graph.py
@@ -1,5 +1,9 @@
+import uuid
 from typing import Any
 
+from langchain_core.runnables import RunnableConfig
+from langgraph.store.base import Item
+from langgraph.store.memory import InMemoryStore
 import pytest
 from dependency_injector.wiring import Provide, inject
 from langchain_mcp_adapters.client import MultiServerMCPClient
@@ -19,6 +23,13 @@ def container() -> Application:
     return container
 
 
+@pytest.fixture
+def basic_runnable_config() -> RunnableConfig:
+    return {
+        "configurable": {"thread_id": str(uuid.uuid4())},
+    }
+
+
 def test_compile_graph():
     graph = make_graph()
     assert isinstance(graph, CompiledGraph)
@@ -29,8 +40,10 @@ def graph() -> CompiledGraph:
     return make_graph()
 
 
-async def test_invoke_graph(graph: CompiledGraph):
-    result: dict[str, Any] = await graph.ainvoke(input=InputState(question="Hello"))
+async def test_invoke_graph(graph: CompiledGraph, basic_runnable_config: RunnableConfig):
+    result: dict[str, Any] = await graph.ainvoke(
+        input=InputState(question="Hello"), config=basic_runnable_config
+    )
 
     validated: OutputState = OutputState.model_validate(result)
     assert validated.response_messages[0].content == "Received: Hello"
@@ -58,3 +71,15 @@ async def test_astream_graph_runner(graph_runner: GraphRunner):
     assert len(updates) > 0
     assert updates[0].type_ == UpdateTypes.graph_start
     assert updates[-1].type_ == UpdateTypes.graph_end
+
+
+async def test_memory_store_standalone():
+    store = InMemoryStore()
+
+    before = await store.aget(namespace=("testing",), key="test")
+    assert before is None
+
+    await store.aput(namespace=("testing",), key="test", value={"value": "value"})
+    after = await store.aget(namespace=("testing",), key="test")
+    assert isinstance(after, Item)
+    assert after.value == {"value": "value"}
